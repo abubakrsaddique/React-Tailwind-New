b9775a021f3b56a99932293d32b72fdf5fa49504
M Abubakar Saddique <abubakarsaddique786.tsk@gmail.com>
2024-07-02 11:19:39 +0500

    Fourth commit

--- src/components/realestatesection/RealEstateSection.jsx
+++ src/components/realestatesection/RealEstateSection.jsx
@@ -106,8 +106,71 @@ const RealEstateSection = () => {
           ></div>
         </div>
       </div>
-
-      <div className="w-[50%]">Form</div>
+      {/* Form */}
+      <div className="w-[50%] flex justify-center items-center col-span-2">
+        <div className="justify-center">
+          <form className="flex flex-col bg-primary rounded-[30px] shadow-[10px_30px_50px_rgba(0,0,0,0.05)] h-[582px]  w-[420px] -mt-20">
+            <h1 className="text-base text-secondary font-medium text-center py-6">
+              Contact Us Now
+            </h1>
+            <div className="flex justify-center">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="344"
+                height="5"
+                viewBox="0 0 344 5"
+                fill="none"
+              >
+                <path
+                  d="M2 1.5L342 2.97399"
+                  stroke="#3A0CA3"
+                  stroke-width="3"
+                  stroke-linecap="round"
+                ></path>
+              </svg>
+            </div>
+            <hr className=""></hr>
+            {/* Form Input */}
+            <div className="flex justify-center items-center h-[90%]">
+              <div className="max-w-[340px]">
+                <input
+                  type="text"
+                  id="name"
+                  name="name"
+                  required=""
+                  class=" bg-[hsla(0,0%,83%,0.2)] border-none rounded-[20px] h-[59px] w-full my-2 px-6 focus:outline-none "
+                  placeholder="Full Name"
+                  value=""
+                />
+                <input
+                  id="email"
+                  name="email"
+                  required=""
+                  class="bg-[hsla(0,0%,83%,0.2)] border-none rounded-[20px] h-[59px] w-full my-2 px-6 focus:outline-none"
+                  type="text"
+                  placeholder="Email Address"
+                  value=""
+                />
+                <textarea
+                  id="message"
+                  name="message"
+                  required=""
+                  class="bg-[hsla(0,0%,83%,0.2)] border-none w-full rounded-[20px] my-2 py-6 px-6"
+                  placeholder="Message"
+                  cols="30"
+                  rows="5"
+                ></textarea>
+                <button
+                  type="btn button"
+                  class="my-4 text-primary bg-secondary rounded-[50px] text-base font-medium w-full h-[60px] hover:bg-[#6d28d9]"
+                >
+                  Submit
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
